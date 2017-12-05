c2818365a1c0bc636f30e3f20bcaf4cd7787b520
alberto <github email address>
2017-12-04 19:46:46 -0500

    reverted to original form with collection_select instead of drop_list

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,9 +5,7 @@ class Game < ApplicationRecord
   has_many :users, through: :planned_games
   accepts_nested_attributes_for :sport, reject_if: proc { |attributes| attributes['name'].blank? }
 
-  validates :location, presence: true
   validates :location, length: { minimum: 5 }
-  validates :title, presence: true
   validates :title, length: { minimum: 5 }
   validates :start_date, presence: true
   validates :start_time, presence: true
