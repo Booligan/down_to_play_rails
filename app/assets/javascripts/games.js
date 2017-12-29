18a29d90ef64d76ba9da3c99b157ae61c829d1b5
alberto <github email address>
2017-12-28 19:26:43 -0500

    successfully requested user game on table

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -3,9 +3,9 @@ $(document).on("turbolinks:load",function(){
 });
 
 function attachGameListeners(){
+
   $('#planned-games').on('click', function(event){
     event.preventDefault();
-
     let userID = $('h1').attr("data-user-id")
     getPlannedGames(userID);
   })
@@ -32,13 +32,34 @@ function getPlannedGames(userID){
                                 <th>Players needed</th>`)
 
     games.forEach(function(game){
+      let maxPlayers = game.max_players
+      let joinedPlayers = game.joined_players.length
+      let playersNeeded = gameCountMessage(maxPlayers, joinedPlayers)
       $('table').append(`<tr>
                           <td><a href="/users/${userID}/games/${game.id}">${game.title}</a></td>
                           <td>${game.sport.name}</td>
                           <td><a href="/users/${userID}">${game.planner.email}</a></td>
+                          ${playersNeeded}
                         </tr>`)
     })
 
   })
+
+  function gameCountMessage(maxPlayers, joinedPlayers){
+    let playersNeeded = maxPlayers - joinedPlayers
+    let message = undefined
+
+    switch(true){
+      case playersNeeded === 0:
+        message = `<td style="color:red">Game is Full</td>`
+        break;
+      case playersNeeded <= 3:
+        message = `<td style="color:yellow">${playersNeeded} players needed.</td>`
+      default:
+        message = `<td style="color:white">${playersNeeded} players needed.</td>`
+    }
+
+    return message;
+  };
 
 };
